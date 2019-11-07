e7d654f1f71fc218054d5a2836741c3e9bff47b9
tyw809611051 <809611051@qq.com>
2019-11-07 17:48:52 +0800

    add 分类右侧布局

--- lib/pages/tabs/Category.dart
+++ lib/pages/tabs/Category.dart
@@ -10,13 +10,24 @@ class CategoryPage extends StatefulWidget {
 
 class _CategoryPageState extends State<CategoryPage> {
   int _selectIndex = 0;
+
+
   @override
   Widget build(BuildContext context) {
+      // 右侧宽度
+  var leftWidth = ScreenAdaper.getScreenWidth()/4;
+  // 右侧宽度=总宽度-左侧宽度-GridView外侧原生padding值-GridView中间的间距
+  var rightItemWidth = (ScreenAdaper.getScreenWidth()-leftWidth-20-20)/3;
+  rightItemWidth = ScreenAdaper.width(rightItemWidth);
+  var rightItemHeight = rightItemWidth+ScreenAdaper.height(28);
+
     return Container(
        child: Row(
          children: <Widget>[
+           
            Container(
-             width: ScreenAdaper.width(140),
+
+             width: leftWidth,
              height: double.infinity,
              child: ListView.builder(
                itemCount: 20,
@@ -43,7 +54,37 @@ class _CategoryPageState extends State<CategoryPage> {
            ),
            Expanded(
              flex: 1,
-             child: Text("我是右侧"),
+             child: Container(
+               padding: EdgeInsets.all(10),
+               height: double.infinity,
+               color: Color.fromRGBO(240, 246, 246, 0.9),
+               child: GridView.builder(
+                 gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                   crossAxisCount: 3,
+                   childAspectRatio: rightItemWidth/rightItemHeight,
+                   crossAxisSpacing: 10,
+                   mainAxisSpacing: 10
+                   ),
+                 itemCount: 16,
+                 itemBuilder: (context,index) {
+                   return Container(
+                    //  padding: EdgeInsets.all(10),
+                     child: Column(
+                       children: <Widget>[
+                         AspectRatio(
+                           aspectRatio: 1/1,
+                           child: Image.network("https://www.itying.com/images/flutter/list8.jpg",fit: BoxFit.cover,),
+                         ),
+                         Container(
+                           height: ScreenAdaper.height(35),
+                           child: Text("女装"),
+                         ),
+                       ],
+                     ),
+                   );
+                 },
+               ),
+             ),
            ),
          ],
        ),
