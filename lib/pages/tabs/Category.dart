48145c1c5044b71216db843092ce43ee811e9528
tyw809611051 <809611051@qq.com>
2019-11-07 17:10:45 +0800

    add 分类左侧布局

--- lib/pages/tabs/Category.dart
+++ lib/pages/tabs/Category.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_cisslab/services/ScreenAdaper.dart';
 
 class CategoryPage extends StatefulWidget {
   CategoryPage({Key key}) : super(key: key);
@@ -8,10 +9,44 @@ class CategoryPage extends StatefulWidget {
 }
 
 class _CategoryPageState extends State<CategoryPage> {
+  int _selectIndex = 0;
   @override
   Widget build(BuildContext context) {
     return Container(
-       child: Text('我是分类组件'),
+       child: Row(
+         children: <Widget>[
+           Container(
+             width: ScreenAdaper.width(140),
+             height: double.infinity,
+             child: ListView.builder(
+               itemCount: 20,
+               itemBuilder: (context,index) {
+                 return Column(
+                   children: <Widget>[
+                     InkWell(
+                       onTap: () {
+                         setState(() {
+                          _selectIndex = index; 
+                         });
+                       },
+                       child: Container(
+                         width: double.infinity,
+                         height: ScreenAdaper.height(56),
+                         child: Text("我是${index}条"),
+                         color: _selectIndex==index?Colors.red:Colors.white,
+                       ),
+                     ),
+                   ],
+                 );
+               },
+             ),
+           ),
+           Expanded(
+             flex: 1,
+             child: Text("我是右侧"),
+           ),
+         ],
+       ),
     );
   }
 }
